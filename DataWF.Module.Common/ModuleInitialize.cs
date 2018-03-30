f7d28d967a8e5c2bbf6ed9062680df6829f6e79b
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-03-30 20:21:32 +0600

    Module Initialize

---
+++ DataWF.Module.Common/ModuleInitialize.cs
@@ -0,0 +1,28 @@
+﻿using DataWF.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataWF.Module.Common
+{
+    public class ModuleInitialize : IModuleInitialize
+    {
+        public void Initialize()
+        {
+            BookType.DBTable.Load();
+            Book.DBTable.Load();
+
+            Department.DBTable.Load();
+            Position.DBTable.Load();
+
+            UserGroup.DBTable.Load();
+            UserGroup.SetCurrent();
+
+            User.DBTable.Load();
+            User.SetCurrent();
+
+            GroupPermission.DBTable.Load();
+            GroupPermission.CachePermission();
+        }
+    }
+}
---
+++ DataWF.Module.Flow/ModuleInitialize.cs
@@ -0,0 +1,19 @@
+﻿using DataWF.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataWF.Module.Flow
+{
+    public class ModuleInitialize : IModuleInitialize
+    {
+        public void Initialize()
+        {
+            Work.DBTable.Load();
+            Stage.DBTable.Load();
+            StageParam.DBTable.Load();
+            Template.DBTable.Load();
+            TemplateParam.DBTable.Load();
+        }
+    }
+}
