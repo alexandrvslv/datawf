deed5c24854bb1ec1c4b26aa33a7e4ca5496d215
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-05-23 17:36:59 +0600

    Blue Theme

--- DataWF.Gui/Styles/GuiThemeList.cs
+++ DataWF.Gui/Styles/GuiThemeList.cs
@@ -7,11 +7,17 @@ namespace DataWF.Gui
         public void GenerateDefault()
         {
             var dark = new GuiTheme { Name = "Dark" };
-            dark.Generate(Font.SystemSansSerifFont, Color.FromBytes(80, 80, 80), -0.06);
+            dark.Generate(Font.SystemSansSerifFont, Color.FromBytes(80, 80, 80), Colors.White, -0.06);
             Add(dark);
+
             var light = new GuiTheme { Name = "Light" };
-            light.Generate(Font.SystemSansSerifFont, Color.FromBytes(190, 190, 190), 0.06);
+            light.Generate(Font.SystemSansSerifFont, Color.FromBytes(190, 190, 190), Colors.Black, 0.06);
             Add(light);
+
+            var blue = new GuiTheme() { Name = "Blue" };
+            blue.Generate(Font.SystemFont, Color.FromName("#4472c4"), Colors.White, -0.05D);
+            //blue["Tool"] = blue.GenerateStyle("Tool", Font.SystemFont.WithSize(Font.SystemFont.Size * 1.08).WithWeight(FontWeight.Semibold), Colors.Red, Colors.White, -0.05D, 5, false, false, false);
+            Add(blue);
         }
     }
 
