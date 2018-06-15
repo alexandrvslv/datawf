34a7a437e6df60b80d75192f0bf255e0315fa923
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-06-15 15:36:31 +0600

    Endpoint Helper

---
+++ DataWF.Common/Net/EndPointHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace DataWF.Common
+{
+    public static class EndPointHelper
+    {
+        public static IEnumerable<IPAddress> GetInterNetworkIPs()
+        {
+            var prop = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties();
+
+            foreach (var ip in Dns.GetHostAddresses(prop.HostName))
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    yield return ip;
+                }
+            }
+        }
+    }
+}
