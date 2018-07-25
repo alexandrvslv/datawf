587682f2ca774f2aae5dc770f6acc70d9a6675c5
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-07-25 19:37:26 +0600

    Fix error when default validation diable

--- DataWF.Web.Common/DBItemValidator.cs
+++ DataWF.Web.Common/DBItemValidator.cs
@@ -1,52 +1,30 @@
 ﻿using DataWF.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using Newtonsoft.Json;
-using System;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace DataWF.Web.Common
 {
-    public class DBItemValidator : IObjectModelValidator
+    public class DBItemValidator : IObjectModelValidator, IClientModelValidator
     {
+        public void AddValidation(ClientModelValidationContext context)
+        {
+
+        }
+
         public void Validate(ActionContext actionContext, ValidationStateDictionary validationState, string prefix, object model)
         {
-            if (model is DBItem)
+            if (!string.IsNullOrEmpty(prefix))
             {
-
+                if (actionContext.ModelState.TryGetValue(prefix, out var validator))
+                {
+                    validator.ValidationState = ModelValidationState.Valid;
+                }
             }
-        }
-    }
-
-    public class ErrorHandlingMiddleware
-    {
-        private readonly RequestDelegate next;
-
-        public ErrorHandlingMiddleware(RequestDelegate next)
-        {
-            this.next = next;
-        }
-
-        public async Task Invoke(HttpContext context /* other dependencies */)
-        {
-            try
-            {
-                await next(context);
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(context, ex);
-            }
-        }
-
-        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
-        {
-            var result = JsonConvert.SerializeObject(new { error = exception.Message });
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)500;
-            return context.Response.WriteAsync(result);
+            //if (model is DBItem)
+            //{
+            //}
         }
     }
 }
