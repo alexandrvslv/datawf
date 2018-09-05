6a020626b3cf3574c501db2dad3ed663a5ed470e
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-09-05 18:27:37 +0600

    Generic EnumItem

--- DataWF.Common/Helper/EnumItem.cs
+++ DataWF.Common/Helper/EnumItem.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
@@ -48,5 +50,83 @@ namespace DataWF.Common
         }
     }
 
+
+    public class EnumItemList<T> : SelectableList<EnumItem<T>> where T : struct
+    {
+        public EnumItemList() : base()
+        { }
+
+        public EnumItemList(IEnumerable<EnumItem<T>> items) : base(items)
+        { }
+
+        public override int IndexOf(object item)
+        {
+            if (item == null)
+                return -1;
+            if (item is T)
+            {
+                return IndexOf((EnumItem<T>)(T)item);
+            }
+            return IndexOf((EnumItem<T>)item);
+        }
+
+        public override object GetItem(int index)
+        {
+            return this[index].Value;
+        }
+    }
+
+    public struct EnumItem<T> where T : struct
+    {
+        static EnumItem()
+        {
+            foreach (T enumItem in Enum.GetValues(typeof(T)))
+            {
+                Cache[enumItem] = new EnumItem<T>(enumItem);
+            }
+        }
+
+        public static Dictionary<T, EnumItem<T>> Cache = new Dictionary<T, EnumItem<T>>();
+
+        public static bool operator ==(T item, EnumItem<T> enumItem) { return item.Equals(enumItem.Value); }
+
+        public static bool operator !=(T item, EnumItem<T> enumItem) { return !item.Equals(enumItem.Value); }
+
+        public static implicit operator T(EnumItem<T> item) { return item.Value; }
+
+        public static implicit operator EnumItem<T>(T item) { return Cache[item]; }
+
+        public EnumItem(T item)
+        {
+            Value = item;
+            Text = item.ToString();
+            var attribute = typeof(T).GetRuntimeField(Text).GetCustomAttribute<EnumMemberAttribute>(false);
+            if (attribute != null)
+                Text = attribute.Value;
+        }
+
+        public T Value { get; set; }
+
+        public string Text { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is EnumItem<T> item)
+            {
+                obj = item.Value;
+            }
+            return Value.Equals(obj);
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
 }
 
