ec0971ed57df1dd427fed6044d6768067cab2e47
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-09-26 18:17:13 +0600

    Memory leak

--- DataWF.Web.Common/Controller/WebNotifyClient.cs
+++ DataWF.Web.Common/Controller/WebNotifyClient.cs
@@ -1,16 +1,23 @@
 ﻿using DataWF.Common;
 using DataWF.Module.Common;
+using System;
 using System.Net.WebSockets;
 
 namespace DataWF.Web.Common
 {
-    public class WebNotifyClient
+    public class WebNotifyClient : IDisposable
     {
         public static readonly Invoker<WebNotifyClient, WebSocket> SocketInvoker = new Invoker<WebNotifyClient, WebSocket>(nameof(Socket), (p) => p.Socket, (p, v) => p.Socket = v);
         public static readonly Invoker<WebNotifyClient, User> UserInvoker = new Invoker<WebNotifyClient, User>(nameof(Socket), (p) => p.User, (p, v) => p.User = v);
 
         public WebSocket Socket { get; set; }
         public User User { get; set; }
+
+        public void Dispose()
+        {
+            Socket?.Dispose();
+            Socket = null;
+        }
     }
 
 }
