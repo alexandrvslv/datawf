75c74a6d661b4545ed2f93cae3fad2c57fb5c607
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-10-01 19:10:20 +0600

    Check expression

--- DataWF.Common/Invoker/PropertyInvoker.cs
+++ DataWF.Common/Invoker/PropertyInvoker.cs
@@ -1,24 +1,26 @@
 ﻿using System;
-using System.Reflection.Emit;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Emit;
 
 namespace DataWF.Common
 {
     public class PropertyInvoker<T, V> : Invoker<T, V>
     {
         public PropertyInvoker(PropertyInfo info)
-            : base(info.Name, GetInvokerGet(info.GetGetMethod()), info.CanWrite ? GetInvokerSet(info.GetSetMethod()) : null)
+            : base(info.Name, GetExpressionGet(info), info.CanWrite ? GetExpressionSet(info) : null)
         { }
 
         public PropertyInvoker(string name)
             : this((PropertyInfo)TypeHelper.GetMemberInfo(typeof(T), name))
         { }
 
-        public static Func<T, V> GetInvokerGet(MethodInfo info)
+        public static Func<T, V> GetEmitGet(MethodInfo info)
         {
             var method = new DynamicMethod(EmitInvoker.GetMethodName(info),
                 typeof(V),
                 new Type[] { typeof(T) },
+                info.DeclaringType,
                 true);
 
             ILGenerator il = method.GetILGenerator();
@@ -40,13 +42,15 @@ namespace DataWF.Common
             return (Func<T, V>)method.CreateDelegate(typeof(Func<T, V>));
         }
 
-        public static Action<T, V> GetInvokerSet(MethodInfo info)
+        public static Action<T, V> GetEmitSet(MethodInfo info)
         {
             if (info == null)
-                return null; 
+                return null;
             DynamicMethod method = new DynamicMethod(EmitInvoker.GetMethodName(info),
                 typeof(void),
-                new Type[] { typeof(T), typeof(V) }, true);
+                new Type[] { typeof(T), typeof(V) },
+                info.DeclaringType,
+                true);
 
             var il = method.GetILGenerator();
             if (!info.IsStatic)
@@ -66,6 +70,27 @@ namespace DataWF.Common
             }
             il.Emit(OpCodes.Ret);
             return (Action<T, V>)method.CreateDelegate(typeof(Action<T, V>));
+        }
+        //public static Action<T,V>
+
+        //https://www.codeproject.com/Articles/584720/Expression-Based-Property-Getters-and-Setters
+        public static Func<T, V> GetExpressionGet(PropertyInfo info)
+        {
+            var paramExpression = Expression.Parameter(typeof(T), "value");
+            var propertyGetterExpression = Expression.Property(paramExpression, info.Name);
+            return Expression.Lambda<Func<T, V>>(propertyGetterExpression, paramExpression).Compile();
+        }
+
+        public static Action<T, V> GetExpressionSet(PropertyInfo info)
+        {
+            var paramTarget = Expression.Parameter(typeof(T));
+            var paramValue = Expression.Parameter(typeof(V), info.Name);
+            var propertyGetterExpression = Expression.Property(paramTarget, info.Name);
+
+            return Expression.Lambda<Action<T, V>>
+            (
+                Expression.Assign(propertyGetterExpression, paramValue), paramTarget, paramValue
+            ).Compile();
         }
     }
 
