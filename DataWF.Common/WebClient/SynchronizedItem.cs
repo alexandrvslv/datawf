1c6b620f49416377e8b6274bb1ef33abc6aecd44
Alexandr Vslv <alexandr_vslv@mail.ru>
2018-10-16 19:08:29 +0600

    Separete notification item from synchronized item

--- DataWF.Common/WebClient/SynchronizedItem.cs
+++ DataWF.Common/WebClient/SynchronizedItem.cs
@@ -1,29 +1,14 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Xml.Serialization;
 
 namespace DataWF.Common
 {
-    public abstract class SynchronizedItem : IContainerNotifyPropertyChanged, INotifyPropertyChanging, ISynchronized
+    public abstract class SynchronizedItem : DefaultItem, ISynchronized
     {
-        public static Action<PropertyChangedEventHandler, object, PropertyChangedEventArgs> GlogalChangedHook;
 
         private bool? isSynchronized = null;
-        private PropertyChangedEventHandler propertyChanged;
-
-        public event PropertyChangedEventHandler PropertyChanged
-        {
-            add => propertyChanged += value;
-            remove => propertyChanged -= value;
-        }
-
-        public event PropertyChangingEventHandler PropertyChanging;
-
-        [JsonIgnore, XmlIgnore]
-        public INotifyListPropertyChanged Container { get; set; }
 
         [JsonIgnore, XmlIgnore]
         public ISet<string> Changes { get; } = new HashSet<string>();
@@ -45,12 +30,7 @@ namespace DataWF.Common
             }
         }
 
-        protected virtual void OnPropertyChanging([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             if (IsSynchronized != null)
             {
@@ -59,19 +39,7 @@ namespace DataWF.Common
 
             Changes.Add(propertyName);
 
-            var arg = new PropertyChangedEventArgs(propertyName);
-            Container?.OnItemPropertyChanged(this, arg);
-            if (propertyChanged != null)
-            {
-                if (GlogalChangedHook != null)
-                {
-                    GlogalChangedHook(propertyChanged, this, arg);
-                }
-                else
-                {
-                    propertyChanged(this, arg);
-                }
-            }
+            base.OnPropertyChanged(propertyName);
         }
     }
 
