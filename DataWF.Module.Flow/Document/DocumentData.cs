1a332f06a0c1832aa07936cc058f972f1947b21f
Alexandr Vassilyev <vassilyev.a@kmgp.kz>
2019-05-08 19:06:07 +0600

    Undo Deletion

--- DataWF.Module.Flow/Document/DocumentData.cs
+++ DataWF.Module.Flow/Document/DocumentData.cs
@@ -96,7 +96,7 @@ namespace DataWF.Module.Flow
         {
             Id = (int)logItem.LogId;
             BaseId = (long)logItem.BaseId;
-            DocumentId = logItem.GetValue<long>(DocumentData.DocumentKey.LogColumn);
+            DocumentId = logItem.GetValue<long?>(DocumentData.DocumentKey.LogColumn);
             Date = (DateTime)logItem.DateCreate;
             Type = (DBLogType)logItem.LogType;
             User = ((UserLog)logItem.UserLog)?.User?.Name;
@@ -423,39 +423,41 @@ namespace DataWF.Module.Flow
         }
 
         [ControllerMethod]
-        public async Task UndoLogFile(int logId, DBTransaction transaction)
+        public static async Task<DocumentData> UndoLogFile(int logId, DBTransaction transaction)
         {
-            var logItem = Table.LogTable.LoadById(logId);
+            var logItem = DBTable.LogTable.LoadById(logId);
             if (logItem == null)
             {
                 throw new Exception($"Not Found!");
             }
 
-            if (!Access.GetFlag(AccessType.Update, transaction.Caller))
+            if (!DBTable.Access.GetFlag(AccessType.Update, transaction.Caller))
             {
                 throw new Exception("Access Denied!");
             }
 
-            await logItem.Undo(transaction);
-            Table.LogTable.Trunc();
+            var data = (DocumentData)await logItem.Undo(transaction);
+            DBTable.LogTable.Trunc();
+            return data;
         }
 
         [ControllerMethod]
-        public async Task RedoLogFile(int logId, DBTransaction transaction)
+        public static async Task<DocumentData> RedoLogFile(int logId, DBTransaction transaction)
         {
-            var logItem = Table.LogTable.LoadById(logId);
+            var logItem = DBTable.LogTable.LoadById(logId);
             if (logItem == null)
             {
                 throw new Exception($"Not Found!");
             }
 
-            if (!Access.GetFlag(AccessType.Update, transaction.Caller))
+            if (!DBTable.Access.GetFlag(AccessType.Update, transaction.Caller))
             {
                 throw new Exception("Access Denied!");
             }
 
-            await logItem.Redo(transaction);
-            Table.LogTable.Trunc();
+            var data = (DocumentData)await logItem.Redo(transaction);
+            DBTable.LogTable.Trunc();
+            return data;
         }
 
         public virtual string RefreshName()
