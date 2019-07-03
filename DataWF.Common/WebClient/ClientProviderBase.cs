cf54627559bb4d748b2424401bd0c8e37fe202e5
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-07-03 20:56:03 +0600

    Fix missing

--- DataWF.Common/WebClient/ClientProviderBase.cs
+++ DataWF.Common/WebClient/ClientProviderBase.cs
@@ -6,12 +6,21 @@ namespace DataWF.Common
     public class ClientProviderBase : IClientProvider
     {
         Dictionary<Type, ICRUDClient> crudClients = new Dictionary<Type, ICRUDClient>();
+        Dictionary<Type, Dictionary<int, ICRUDClient>> crudTypedClients = new Dictionary<Type, Dictionary<int, ICRUDClient>>();
         private SelectableList<IClient> clients = new SelectableList<IClient>();
 
         public ClientProviderBase()
         {
 
         }
+
+        public AuthorizationInfo Authorization { get; set; }
+
+        public string BaseUrl { get; set; }
+
+        public SelectableList<IClient> Clients => clients;
+
+        IEnumerable<IClient> IClientProvider.Clients => Clients;
 
         protected void Add(IClient client)
         {
@@ -21,13 +30,30 @@ namespace DataWF.Common
                 crudClients[crudClient.ItemType] = crudClient;
             }
         }
-        public AuthorizationInfo Authorization { get; set; }
 
-        public string BaseUrl { get; set; }
-
-        public SelectableList<IClient> Clients => clients;
-
-        IEnumerable<IClient> IClientProvider.Clients => Clients;
+        public void RefreshTypedCache()
+        {
+            foreach (var crudClient in clients.TypeOf<ICRUDClient>())
+            {
+                if (crudClient.TypeId != 0)
+                {
+                    var baseType = crudClient.ItemType.BaseType;
+                    var baseClien = (ICRUDClient)null;
+                    while (baseType != null && ((baseClien = GetClient(baseType)) == null || baseClien.TypeId != 0))
+                    {
+                        baseType = baseType.BaseType;
+                    }
+                    if (baseClien != null)
+                    {
+                        if (!crudTypedClients.TryGetValue(baseType, out var types))
+                        {
+                            crudTypedClients[baseType] = types = new Dictionary<int, ICRUDClient>();
+                        }
+                        types[crudClient.TypeId] = crudClient;
+                    }
+                }
+            }
+        }
 
         public ICRUDClient<T> GetClient<T>()
         {
@@ -41,7 +67,17 @@ namespace DataWF.Common
 
         public ICRUDClient GetClient(Type type, int typeId)
         {
-            throw new NotImplementedException();
+            if (crudTypedClients.Count == 0)
+            {
+                RefreshTypedCache();
+            }
+            var baseType = type;
+            Dictionary<int, ICRUDClient> types = null;
+            while (baseType != null && !crudTypedClients.TryGetValue(baseType, out types))
+            {
+                baseType = baseType.BaseType;
+            }
+            return types != null && types.TryGetValue(typeId, out var client) ? client : null;
         }
     }
 }
