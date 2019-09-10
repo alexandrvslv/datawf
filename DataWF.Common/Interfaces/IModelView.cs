509b4aa91a187a13d24c16190fed61318a8c7336
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-09-10 15:19:46 +0600

    Extend Interface

--- DataWF.Common/Interfaces/IModelView.cs
+++ DataWF.Common/Interfaces/IModelView.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DataWF.Common
@@ -7,6 +8,8 @@ namespace DataWF.Common
     {
         ICollection Items { get; set; }
         object SelectedItem { get; set; }
+        IEnumerable<object> SelectedItems { get; set; }
+
         Task Save(object item);
         Task Save(object item, ProgressToken token);
 
