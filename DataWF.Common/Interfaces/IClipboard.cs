e6842426fb3995f248d069946415291635a9ce1a
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-10-30 21:06:46 +0600

    Clipboard interface

---
+++ DataWF.Common/Interfaces/IClipboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DataWF.Common
+{
+    /// <summary>
+    /// https://github.com/xamarin/Essentials/blob/master/Xamarin.Essentials/Clipboard/Clipboard.shared.cs
+    /// await WPF Implementation
+    /// </summary>
+    public interface IClipboard
+    {
+        Task SetTextAsync(string text);
+
+        bool HasText { get; }
+
+        Task<string> GetTextAsync();
+    }
+}
