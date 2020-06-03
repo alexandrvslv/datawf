e9c513c2836bce708f4a688da2208be569cfd301
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2020-06-03 17:18:52 +0600

    Fix sorting

--- DataWF.WebService.Common/Controller/BaseLogedController.cs
+++ DataWF.WebService.Common/Controller/BaseLogedController.cs
@@ -23,12 +23,13 @@ namespace DataWF.WebService.Common
                 {
                     return Forbid();
                 }
-                if (table is IDBVirtualTable virtualTable && virtualTable.ItemTypeIndex != 0)
-                {
-                    filter = $"({filter}) and {virtualTable.ItemTypeKey?.Property} = {virtualTable.ItemTypeIndex}";
-                }
+                
                 if (!logTable.ParseQuery(filter, out var query))
                 {
+                    if (table is IDBVirtualTable virtualTable && virtualTable.ItemTypeIndex != 0)
+                    {
+                        query.BuildParam(logTable.ItemTypeKey, virtualTable.ItemTypeIndex);                        
+                    }
                     var buffer = logTable.LoadItems(query, DBLoadParam.Referencing)
                                      .Where(p => p.Access.GetFlag(AccessType.Read, user)
                                                  && p.PrimaryId != null
