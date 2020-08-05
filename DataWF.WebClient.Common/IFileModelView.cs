b17abb5787cfb99fd3ba09664cdc5790372804dc
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2020-08-05 10:15:49 +0600

    Change Interface

--- DataWF.WebClient.Common/IFileModelView.cs
+++ DataWF.WebClient.Common/IFileModelView.cs
@@ -9,7 +9,7 @@ namespace DataWF.Common
         IFileClient FileClient { get; }
         Task<string> SilentDownload(IFileModel item);
         Task<string> Download(IFileModel item);
-        Task<FileWatcher> Edit(IFileModel item);
+        Task<bool> Edit(IFileModel item);
         Task<Stream> Open(IFileModel item);
         Task<object> Save(IFileModel item);
         Task<object> SaveAs(IFileModel item);
