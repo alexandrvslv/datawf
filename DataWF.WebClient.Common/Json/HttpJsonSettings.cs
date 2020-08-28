e5d18d7069495e81a96436806908b146875ae33d
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2020-08-28 12:43:19 +0600

    Handly constants

--- DataWF.WebClient.Common/Json/HttpJsonSettings.cs
+++ DataWF.WebClient.Common/Json/HttpJsonSettings.cs
@@ -6,8 +6,12 @@ namespace DataWF.Common
     {
         public static readonly HttpJsonSettings Default = new HttpJsonSettings(HttpJsonKeys.Full);
         public static readonly HttpJsonSettings None = new HttpJsonSettings(HttpJsonKeys.None);
-        public static readonly HttpJsonSettings OnlyReferencing = new HttpJsonSettings(HttpJsonKeys.Refing | HttpJsonKeys.Ref);
-        public static readonly HttpJsonSettings OnlyReferenced = new HttpJsonSettings(HttpJsonKeys.Refed | HttpJsonKeys.Ref);
+        public static readonly HttpJsonSettings OnlyReferencing2Level = new HttpJsonSettings(HttpJsonKeys.Refing | HttpJsonKeys.Ref, 2);
+        public static readonly HttpJsonSettings OnlyReferencing3Level = new HttpJsonSettings(HttpJsonKeys.Refing | HttpJsonKeys.Ref, 3);
+        public static readonly HttpJsonSettings OnlyReferencing4Level = new HttpJsonSettings(HttpJsonKeys.Refing | HttpJsonKeys.Ref, 4);
+        public static readonly HttpJsonSettings OnlyReferenced2Level = new HttpJsonSettings(HttpJsonKeys.Refed | HttpJsonKeys.Ref, 2);
+        public static readonly HttpJsonSettings OnlyReferenced3Level = new HttpJsonSettings(HttpJsonKeys.Refed | HttpJsonKeys.Ref, 3);
+        public static readonly HttpJsonSettings OnlyReferenced4Level = new HttpJsonSettings(HttpJsonKeys.Refed | HttpJsonKeys.Ref, 4);
 
         [Obsolete]
         public static readonly string JsonKeys = "json_keys";
