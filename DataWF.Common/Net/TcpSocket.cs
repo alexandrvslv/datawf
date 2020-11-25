7e7ee8047e24193b1db8f89667ea983fad7ed0d2
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2020-11-25 19:57:53 +0600

    Fix missing

--- DataWF.Common/Net/TcpSocket.cs
+++ DataWF.Common/Net/TcpSocket.cs
@@ -219,6 +219,8 @@ namespace DataWF.Common
 #if NETSTANDARD2_0
                 await arg.WriterStream.WriteAsync(buffer.ToArray(), 0, buffer.Length);
 #else
+                var memory = arg.Pipe.Writer.GetMemory(buffer.Length);
+                buffer.CopyTo(memory);
                 arg.Pipe.Writer.Advance(buffer.Length);
                 var result = await arg.Pipe.Writer.FlushAsync();
 #endif
